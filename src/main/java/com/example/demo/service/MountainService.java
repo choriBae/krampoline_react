a95f8bf04cda961d64023882f43685c2a467a1b9
Chori <83741413+choriBae@users.noreply.github.com>
2024-02-29 05:39:02 +0900

    Delete src directory

--- src/main/java/com/example/demo/service/MountainService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-import java.util.Map;
-
-public interface MountainService {
-    String createMountain();
-}
--- src/main/java/com/example/demo/service/WeatherService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-import java.util.Map;
-
-public interface WeatherService {
-    Map<String, Object> createWeather(Float latitude, Float longtitude);
-}
